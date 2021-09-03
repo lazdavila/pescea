65a295b29e995fd2e02ff60983a0688680732cae
lazdavila <ldavila@davtec.com.au>
2021-09-03 22:18:19 +0930

    Package name change

--- setup.py
+++ setup.py
@@ -7,26 +7,28 @@ with open("README.md", "r") as fh:
 
 setup(
     name="python-escea",
-    version="1.0",
+    version="1.0.0",
     author="Laz Davila",
     author_email="laz.davila@gmail.com",
     description="A python interface to the Escea fireplace controllers",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    keywords=['Escea', 'IoT', ],
+    keywords=[
+        "Escea",
+        "IoT",
+    ],
     url="https://github.com/lazdavila/python-pescea",
-    python_requires='~=3.8',
-    install_requires=['asyncio'],
-    tests_require=['pytest'],
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    python_requires="~=3.8",
+    install_requires=["asyncio"],
+    tests_require=["pytest"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: "
-            "GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: " "GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Topic :: Home Automation",
-        "Topic :: System :: Hardware"
+        "Topic :: System :: Hardware",
     ],
 )
